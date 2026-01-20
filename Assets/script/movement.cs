f0077d325c8a407f254c405cb7728469b43123bc
ZomboriLaszlo <zombori.laszlo@simonyiszki.org>
2026-01-20 16:18:47 +0100

    animacio

--- Assets/script/movement.cs
+++ Assets/script/movement.cs
@@ -4,12 +4,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public bool canMove = true;   // EZ A KULCS
+    [SerializeField] private Animator Animator;
 
     private Rigidbody2D rb;
-
+    private SpriteRenderer sr;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
@@ -18,5 +20,16 @@ public class PlayerMovement : MonoBehaviour
 
         float move = Input.GetAxisRaw("Horizontal");
         rb.MovePosition(rb.position + Vector2.right * move * speed * Time.fixedDeltaTime);
+
+        if (move != 0)
+        {
+            Animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            Animator.SetBool("isRunning", false);
+        }
+        if (move < 0) sr.flipX = true;
+        else if (move > 0) sr.flipX = false;
     }
 }
